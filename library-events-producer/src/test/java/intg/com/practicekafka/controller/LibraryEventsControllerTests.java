1e63007cc59942906f8daf68759dec09f0bca2fb
Patrick <patricktsaikl@gmail.com>
2024-11-12 16:36:49 +0800

    test/integration test for post library event

---
+++ library-events-producer/src/test/java/intg/com/practicekafka/controller/LibraryEventsControllerTests.java
@@ -0,0 +1,40 @@
+package com.practicekafka.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.practicekafka.LibraryEventsProducerApplication;
+import com.practicekafka.domain.LibraryEvent;
+import com.practicekafka.util.TestUtil;
+
+// It's an integration test, so a real API is going to be sent.
+// kafka dockers should be up for integration test
+
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = LibraryEventsProducerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void postLibraryEvent() {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("content-type", MediaType.APPLICATION_JSON.toString());
+        var httpEntity = new HttpEntity<>(TestUtil.libraryEventRecord(),
+                httpHeaders);
+        var responseEntity = restTemplate.exchange("/v1/libraryevent", HttpMethod.POST, httpEntity, LibraryEvent.class);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+    }
+}
