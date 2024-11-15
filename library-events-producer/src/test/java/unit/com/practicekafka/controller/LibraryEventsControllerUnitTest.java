cdedb90a78d5ef604377b06c96796712b6025028
Patrick <patricktsaikl@gmail.com>
2024-11-15 11:05:46 +0800

    test/unit test for postLibraryEvent

---
+++ library-events-producer/src/test/java/unit/com/practicekafka/controller/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,45 @@
+package com.practicekafka.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practicekafka.domain.LibraryEvent;
+import com.practicekafka.producer.LibraryEventsProducer;
+import com.practicekafka.util.TestUtil;
+
+//@SpringBootTest -> to bring up the spring boot app in test which is not needed in unit tests
+@WebMvcTest(LibraryEventsController.class)
+public class LibraryEventsControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    LibraryEventsProducer libraryEventsProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+        var json = objectMapper.writeValueAsString(TestUtil.libraryEventRecord());
+        when(libraryEventsProducer.sendLibraryEventProducerRecord(isA(LibraryEvent.class)))
+                .thenReturn(null);
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders.post("/v1/libraryevent")
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+}
