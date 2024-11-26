118a78fff398524f2d7f117affb83ee532b55de7
Patrick <patricktsaikl@gmail.com>
2024-11-26 17:05:25 +0800

    feat/setup scheduler to query failed records in db and retry them

--- library-events-consumer/src/main/java/com/practicekafka/jpa/FailureRecordRepository.java
+++ library-events-consumer/src/main/java/com/practicekafka/jpa/FailureRecordRepository.java
@@ -1,9 +1,12 @@
 package com.practicekafka.jpa;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.practicekafka.entity.FailureRecord;
 
 public interface FailureRecordRepository extends CrudRepository<FailureRecord, Integer> {
 
+    List<FailureRecord> findAllByStatus(String retry);
 }
